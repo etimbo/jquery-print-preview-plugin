7ef86270d439b5f0a9fd8439f950866c25ccf564
Tim Connell <etimbo@gmail.com>
2012-01-14 20:21:04 +1100

    Resolve issue (FF only previewing once) - see https://github.com/etimbo/jquery-print-preview-plugin/issues/1

--- src/jquery.print-preview.js
+++ src/jquery.print-preview.js
@@ -42,7 +42,12 @@
                 .appendTo('body');
 
             // The frame lives
-            var print_frame_ref = window.frames["print-frame"].document;
+            for (var i=0; i < window.frames.length; i++) {
+                if (window.frames[i].name == "print-frame") {    
+                    var print_frame_ref = window.frames[i].document;
+                    break;
+                }
+            }
             print_frame_ref.open();
             print_frame_ref.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
                 '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">' + 
