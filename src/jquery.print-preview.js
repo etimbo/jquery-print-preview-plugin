2159228ba8d12074181d28dc977c96e7ce125522
Tim Connell <etimbo@gmail.com>
2012-01-25 13:45:31 +1100

    Roll v1.0.1 of plugin in to demo

--- src/jquery.print-preview.js
+++ src/jquery.print-preview.js
@@ -1,27 +1,27 @@
 /*!
- * jQuery Print Previw Plugin v0.1
+ * jQuery Print Previw Plugin v1.0.1
  *
  * Copyright 2011, Tim Connell
  * Licensed under the GPL Version 2 license
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * Date: Sun Mar 13 00:00:00 2011 -000
+ * Date: Wed Jan 25 00:00:00 2012 -000
  */
  
 (function($) { 
     
-	// Initialization
-	$.fn.printPreview = function() {
-		this.each(function() {
-			$(this).bind('click', function(e) {
-			    e.preventDefault();
-			    if (!$('#print-modal').length) {
-			        $.printPreview.loadPrintPreview();
-			    }
-			});
-		});
-		return this;
-	};
+    // Initialization
+    $.fn.printPreview = function() {
+        this.each(function() {
+            $(this).bind('click', function(e) {
+                e.preventDefault();
+                if (!$('#print-modal').length) {
+                    $.printPreview.loadPrintPreview();
+                }
+            });
+        });
+        return this;
+    };
     
     // Private functions
     var mask, size, print_modal, print_controls;
@@ -42,7 +42,12 @@
                 .appendTo('body');
 
             // The frame lives
-            var print_frame_ref = window.frames["print-frame"].document;
+            for (var i=0; i < window.frames.length; i++) {
+                if (window.frames[i].name == "print-frame") {    
+                    var print_frame_ref = window.frames[i].document;
+                    break;
+                }
+            }
             print_frame_ref.open();
             print_frame_ref.write('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
                 '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">' + 
@@ -52,10 +57,23 @@
             print_frame_ref.close();
             
             // Grab contents and apply stylesheet
-            $('body', print_frame_ref).html($('body > *:not(#print-modal):not(script)').clone());
-            $('head link[media*=print], head link[media=all]').each(function() {
-                $('head', print_frame_ref).append($(this).clone().attr('media', 'all'));
+            var $iframe_head = $('head link[media*=print], head link[media=all]').clone(),
+                $iframe_body = $('body > *:not(#print-modal):not(script)').clone();
+            $iframe_head.each(function() {
+                $(this).attr('media', 'all');
             });
+            if (!$.browser.msie && !($.browser.version < 7) ) {
+                $('head', print_frame_ref).append($iframe_head);
+                $('body', print_frame_ref).append($iframe_body);
+            }
+            else {
+                $('body > *:not(#print-modal):not(script)').clone().each(function() {
+                    $('body', print_frame_ref).append(this.outerHTML);
+                });
+                $('head link[media*=print], head link[media=all]').each(function() {
+                    $('head', print_frame_ref).append($(this).clone().attr('media', 'all')[0].outerHTML);
+                });
+            }
             
             // Disable all links
             $('a', print_frame_ref).bind('click.printPreview', function(e) {
@@ -105,70 +123,70 @@
                 if ($(this).hasClass('print')) { window.print(); }
                 else { $.printPreview.distroyPrintPreview(); }
             });
-    	},
-    	
-    	distroyPrintPreview: function() {
-    	    print_controls.fadeOut(100);
-    	    print_modal.animate({ top: $(window).scrollTop() - $(window).height(), opacity: 1}, 400, 'linear', function(){
-    	        print_modal.remove();
-    	        $('body').css({overflowY: 'auto', height: 'auto'});
-    	    });
-    	    mask.fadeOut('slow', function()  {
-    			mask.remove();
-    		});				
+        },
+        
+        distroyPrintPreview: function() {
+            print_controls.fadeOut(100);
+            print_modal.animate({ top: $(window).scrollTop() - $(window).height(), opacity: 1}, 400, 'linear', function(){
+                print_modal.remove();
+                $('body').css({overflowY: 'auto', height: 'auto'});
+            });
+            mask.fadeOut('slow', function()  {
+                mask.remove();
+            });             
 
-    		$(document).unbind("keydown.printPreview.mask");
-    		mask.unbind("click.printPreview.mask");
-    		$(window).unbind("resize.printPreview.mask");
-	    },
-	    
-    	/* -- Mask Functions --*/
-	    loadMask: function() {
-	        size = $.printPreview.sizeUpMask();
+            $(document).unbind("keydown.printPreview.mask");
+            mask.unbind("click.printPreview.mask");
+            $(window).unbind("resize.printPreview.mask");
+        },
+        
+        /* -- Mask Functions --*/
+        loadMask: function() {
+            size = $.printPreview.sizeUpMask();
             mask = $('<div id="print-modal-mask" />').appendTo($('body'));
-    	    mask.css({				
-    			position:           'absolute', 
-    			top:                0, 
-    			left:               0,
-    			width:              size[0],
-    			height:             size[1],
-    			display:            'none',
-    			opacity:            0,					 		
-    			zIndex:             9999,
-    			backgroundColor:    '#000'
-    		});
-	
-    		mask.css({display: 'block'}).fadeTo('400', 0.75);
-    		
+            mask.css({              
+                position:           'absolute', 
+                top:                0, 
+                left:               0,
+                width:              size[0],
+                height:             size[1],
+                display:            'none',
+                opacity:            0,                          
+                zIndex:             9999,
+                backgroundColor:    '#000'
+            });
+    
+            mask.css({display: 'block'}).fadeTo('400', 0.75);
+            
             $(window).bind("resize..printPreview.mask", function() {
-				$.printPreview.updateMaskSize();
-			});
-			
-			mask.bind("click.printPreview.mask", function(e)  {
-				$.printPreview.distroyPrintPreview();
-			});
-			
-			$(document).bind("keydown.printPreview.mask", function(e) {
-			    if (e.keyCode == 27) {  $.printPreview.distroyPrintPreview(); }
-			});
+                $.printPreview.updateMaskSize();
+            });
+            
+            mask.bind("click.printPreview.mask", function(e)  {
+                $.printPreview.distroyPrintPreview();
+            });
+            
+            $(document).bind("keydown.printPreview.mask", function(e) {
+                if (e.keyCode == 27) {  $.printPreview.distroyPrintPreview(); }
+            });
         },
     
         sizeUpMask: function() {
             if ($.browser.msie) {
-            	// if there are no scrollbars then use window.height
-            	var d = $(document).height(), w = $(window).height();
-            	return [
-            		window.innerWidth || 						// ie7+
-            		document.documentElement.clientWidth || 	// ie6  
-            		document.body.clientWidth, 					// ie6 quirks mode
-            		d - w < 20 ? w : d
-            	];
+                // if there are no scrollbars then use window.height
+                var d = $(document).height(), w = $(window).height();
+                return [
+                    window.innerWidth ||                        // ie7+
+                    document.documentElement.clientWidth ||     // ie6  
+                    document.body.clientWidth,                  // ie6 quirks mode
+                    d - w < 20 ? w : d
+                ];
             } else { return [$(document).width(), $(document).height()]; }
         },
     
         updateMaskSize: function() {
-    		var size = $.printPreview.sizeUpMask();
-    		mask.css({width: size[0], height: size[1]});
+            var size = $.printPreview.sizeUpMask();
+            mask.css({width: size[0], height: size[1]});
         }
     }
 })(jQuery);
